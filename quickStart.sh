574a6f11ba970cd6739b98baa2b98b4e8af7ab8f
dsong127 <dsong127@gmail.com>
2019-06-13 15:05:53 -0700

    stop tracking quickstart

--- quickStart.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# replace with your keys from Spotify dashboard
-export SPOTIFY_CLIENT_ID='fa7144ecf3e340808ac7c02082b98236'
-export SPOTIFY_CLIENT_SECRET='c214f745c9f84af3ac9ae3a60d021f88'
-
-nodemon app.js
