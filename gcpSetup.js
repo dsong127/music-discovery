3d292b33e3e8bd932934c38be0ffd4995065a4f3
dsong127 <dsong127@gmail.com>
2019-07-17 18:19:38 -0700

    fixing gcp setupt

--- gcpSetup.js
+++ gcpSetup.js
@@ -1,6 +1,8 @@
 const fs = require ('fs');
 
-fs.writeFile(process.env.GCP_KEY_FILE, process.env.GCP_CRED, (ERR) => {
-	if (err) throw err;
-  console.log('The file has been saved!');
+fs.writeFile(process.env.GCP_KEY_FILE, process.env.GCP_CRED, (err) => {
+	if (err) {
+		throw err;
+	}
+	console.log('The file has been saved!');
 });
