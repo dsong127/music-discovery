efd90df97e57988dd985a89baefa2f0069f1f8da
dsong127 <dsong127@gmail.com>
2019-07-17 18:04:57 -0700

    fix gcloud setup

--- gcpSetup.js
+++ gcpSetup.js
@@ -1,3 +1,5 @@
 var fs=require(‘fs’);
 
-fs.writeFile(process.env.GCP_KEY_FILE, process.env.GCP_CRED, (err) => {});
+fs.writeFile(process.env.GCP_KEY_FILE, process.env.GCP_CRED, (err) => {
+	console.log(err);
+});
