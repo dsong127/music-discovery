9fca8e9dd02d9b38d123fa7a4cd6cba711b0aa1f
dsong127 <dsong127@gmail.com>
2019-07-19 15:15:54 -0700

    vision auth on heroku

--- visionSetup.js
+++ visionSetup.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
+const path = require('path');
 
-fs.writeFile('vision_creds.json', process.env.SERVICE_ACCOUNT_JSON, (err) => {
+
+fs.writeFile(path.join(__dirname, 'vision_creds.json'), process.env.SERVICE_ACCOUNT_JSON, (err) => {
 	if (err) throw err;
 	console.log('Vision API credentials file has been saved!');
 });
